5a6642397265257ae4ec5215b58b90a644f053f9
The Invisible Man <granados.carlos91@gmail.com>
2017-06-01 02:33:59 -0400

    comments

--- database.py
+++ database.py
@@ -39,30 +39,40 @@ class Cluster:
 
     def start_up(self):
 
+        # Open the user supplied file
         with open(self.file_location) as file:
+            # Use the csv reader
             reader = csv.reader(file, delimiter=',')
             node_counter = 0
             line = 0
             node = self.init_node(node_counter)
+
+            # Let's get the column names and positions
             self.column_names = next(reader)
 
             try:
+                # Find the position of the indexed column
                 self.index_pos = self.column_names.index(self.index_col)
             except:
                 raise "Invalid index, column with name " + self.index_col + " does not exist in this data set"
 
             for row in reader:
 
+                # Check if we should cut off the data and start a new node yet
                 if line == self.MAX_NODE_SIZE:
                     node.close()
                     node_counter += 1
                     line = 0
                     node = self.init_node(node_counter)
 
-                key = self.norm(row[self.index_pos])
-                self.track(key, node_counter, line)
+                # Call track() to create an in memory index that points to
+                # the current node and line number.
+                self.track(row[self.index_pos], node_counter, line)
+
+                # Write to database storage as raw text
                 node.write(','.join(row) + '\n')
 
+                # Keep a count of the total records so far, and the total lines in this current node
                 self.total_records += 1
                 line += 1
 
@@ -74,11 +84,16 @@ class Cluster:
         return path
 
     def track(self, key, node, line):
+        # Remove non alpha numeric characters and lowercase
+
+        key = self.norm(key)
+        # The Index tuple will help us track the
         i = Index(node=node, line=line)
 
         if key not in self.HashMap:
             self.HashMap[key] = list()
 
+        # Add Index object to bucket
         self.HashMap[key].append(i)
 
     def norm(self, key):
@@ -96,18 +111,23 @@ class Cluster:
     def search(self, key):
         key = self.norm(key)
 
+        # Because we are looking for exact matches we just check if the key exists in our dictionary
         if key in self.HashMap:
 
             indexes = self.HashMap[key]
 
             for i in indexes:
+                # Yielding results in less waiting, we daisy chain the parent function so that it can build the result
+                # table at the same rate as the results come in.
                 yield self.fetch(i)
 
     def validate_storage(self, storage):
 
         if os.path.exists(storage):
+            # Clear data from last session
             shutil.rmtree(storage)
 
+        # Start fresh storage folder
         os.makedirs(storage)
 
         return storage
